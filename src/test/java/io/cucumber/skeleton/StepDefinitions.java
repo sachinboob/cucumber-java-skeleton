28dae7a3ed17db83cb937c87b161e2ab0a79c7d4
Sachin Boob <boob.sachin@gmail.com>
2020-09-06 21:01:12 +0530

    Update StepDefinitions.java

--- src/test/java/io/cucumber/skeleton/StepDefinitions.java
+++ src/test/java/io/cucumber/skeleton/StepDefinitions.java
@@ -1,10 +1,18 @@
 package io.cucumber.skeleton;
 
 import io.cucumber.java.en.Given;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class StepDefinitions {
     @Given("I have {int} cukes in my belly")
     public void I_have_cukes_in_my_belly(int cukes) {
+        
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        
         Belly belly = new Belly();
         belly.eat(cukes);
     }
